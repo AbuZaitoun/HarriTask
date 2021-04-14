f1752ad999268e89c76df005884c694ae59e0901
AbuZaitoun <anasomarzaitoun@gmail.com>
2021-04-14 10:18:35 +0300

    started documenting

--- HarriIosTask/Models/User.swift
+++ HarriIosTask/Models/User.swift
@@ -6,19 +6,36 @@
 //
 import Foundation
 
+/// User
 struct User: Codable {
+    
+    /// First name
     var firstName: String
+    
+    /// Last name
     var lastName: String
+    
+    /// Id
     var id: Int
+    
+    /// Is first job
     var isFirstJob: Bool?
+    
+    /// Position
     var position: UserPosition?
+    
+    /// Location
     var location: UserLocation
+    
+    /// Profile image uuid
     var profileImageUUID: String?
     
+    /// Full name
     var fullName: String {
         return self.firstName + " " + self.lastName
     }
     
+    /// Image url
     var imageURL: String? {
         if let uuid = self.profileImageUUID {
         return "https://d7f8bv52wga7t.cloudfront.net/users/\(id)/user_profile/\(uuid)/240_240.jpeg"
@@ -26,6 +43,18 @@ struct User: Codable {
         else {return nil}
     }
     
+    /** Initializer
+
+     */
+    init() {
+        self.firstName = ""
+        self.lastName = ""
+        self.id = -1
+        self.location = UserLocation()
+    }
+    
+    // MARK: - Codable
+    /// Coding keys
     enum CodingKeys: String, CodingKey {
         case firstName
         case lastName
@@ -38,19 +67,33 @@ struct User: Codable {
 }
 
 
+/// Users
 struct Users: Codable {
+    
+    /// Hits
     var hits: Int
+    
+    /// All
     var all: [User]
     
+    // MARK: - Codable
+    
+    /// Coding keys
     enum CodingKeys: String, CodingKey {
         case hits
         case all = "results"
     }
 }
 
+/// Response Data
 struct ResponseData: Codable {
+    
+    /// Data
     var data: Users
     
+    // MARK: - Codable
+    
+    /// Coding keys
     enum CodingKeys: String, CodingKey {
         case data
     }
--- HarriIosTask/Models/UserData.swift
+++ HarriIosTask/Models/UserData.swift
@@ -5,18 +5,34 @@
 //  Created by Anas AbuZaitoun on 05/04/2021.
 //
 
+
+/// User Position
 struct UserPosition: Codable {
+    
+    /// Name
     var name: String
     
+    // MARK: - Codable
+    
+    /// Coding keys
     enum CodingKeys: String, CodingKey {
         case name
     }
 }
 
+
+/// User Location
 struct UserLocation: Codable {
+    
+    /// City
     var city: String?
+    
+    /// State
     var state: String?
     
+    // MARK: - Codable
+    
+    /// Coding keys
     enum CodingKeys: String, CodingKey {
         case city, state
     }
