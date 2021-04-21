b5dfae59a734dc67a1ce9c5499ce03837184c3c8
AbuZaitoun <anasomarzaitoun@gmail.com>
2021-04-21 11:31:30 +0300

    Documented conformance of Label Protocol

--- HarriIosTask/Extensions/UserInfoTableViewController+UserInfoAboutCellDelegate.swift
+++ HarriIosTask/Extensions/UserInfoTableViewController+UserInfoAboutCellDelegate.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
+// MARK: - UserInfoAboutCellDelegate Protocol Conformance
+
 extension UserInfoTableViewController: UserInfoAboutCellDelegate {
+    
+    /// Label clicked
+    /// - Parameter indexPath: IndexPath
     func labelClicked(indexPath: IndexPath) {
-        let cell = self.tableView.cellForRow(at: indexPath) as! UserInfoAboutTableViewCell
         
+        let cell = self.tableView.cellForRow(at: indexPath) as! UserInfoAboutTableViewCell
         cell.setupCell(with: aboutViewModel.representableForRow(at: indexPath) as! UserInfoAboutRepresentable, shouldExpand: true)
         self.tableView.reloadRows(at: [IndexPath(item: indexPath.row, section: 0)], with: .automatic)
     }
